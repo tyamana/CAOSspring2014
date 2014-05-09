aafe815b3e879aa0c430ad44d66486a3f41764e1
tyamana <tyamana@mail.ru>
2014-05-09 23:28:11 +0400

    chat

---
+++ client.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/select.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define ERRSOCKET -1
+#define ERRCONNECT -2
+#define ERRSELECT -3
+
+void PrintError(char* s, int errNum)
+{
+	perror(s);
+	_exit(errNum);
+}
+
+int main(int argc, char** argv)
+{
+	struct sockaddr_in sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = PF_INET;
+	sa.sin_port = htons(4400);
+	sa.sin_addr.s_addr = INADDR_ANY;
+	
+	int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(sock < 0)
+		PrintError("socket() error", ERRSOCKET);
+	if(connect(sock, (struct sockaddr*)&sa, sizeof(sa)) < 0)
+		PrintError("connect() error", ERRCONNECT);
+		
+	write(1, "Connected to server\n", 20);
+		
+	////Non-blocking input
+	//int flags = fcntl(sock, F_GETFL, 0);
+	//if (flags == -1) flags = 0;
+	//fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+	
+	char buf[1024];
+	while(1)
+	{
+		fd_set rdset, wrset;
+		FD_ZERO(&wrset);
+		FD_ZERO(&rdset);
+		FD_SET(0, &rdset);
+		FD_SET(sock, &rdset);
+		
+		int n = select(sock + 1, &rdset, NULL, NULL, NULL);
+		if(n < 0)
+			PrintError("select() error", ERRSELECT);
+		if(n > 0)
+		{
+			if(FD_ISSET(sock, &rdset))
+			{
+				int bc = read(sock, buf, 1024);
+				write(1, buf, bc);
+			}
+			if(FD_ISSET(0, &rdset))
+			{
+				int bc = read(0, buf, 1024);
+				if(FD_ISSET(sock, &wrset) == 0)
+				{
+					fd_set setForSock;
+					FD_SET(sock, &setForSock);
+					if(select(sock, NULL, &setForSock, NULL, NULL) < 0) 
+						PrintError("select() error", ERRSELECT);
+				}
+				write(sock, buf, bc);
+			}
+		}
+	}
+		
+	close(sock);
+	
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <sys/select.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define ERRSOCKET -1
+#define ERRBIND -2
+#define ERRLISTEN -3
+#define ERRSELECT -4
+#define ERRACCEPT -5
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+fd_set fds;
+int fdID[100];
+char names[100][20];
+int fdNum = 0;
+int maxFD = 0; //for using in select()
+struct sockaddr_in sa;
+int sock;
+pthread_t threads[2];
+
+void PrintError(char* s, int errNum)
+{
+	close(sock);
+	pthread_mutex_destroy(&m);
+	perror(s);
+	_exit(errNum);
+}
+
+int max(int a, int b)
+{
+	if(a > b)
+		return a;
+	else
+		return b;
+}
+
+void* Accepting(void* p)
+{
+	FD_ZERO(&fds);
+	write(1, "Server is working\n", 18);
+	write(1, "To turn it off enter \"quit\"\n", 28);
+	while(1)
+	{
+		struct sockaddr saddr;
+		socklen_t sl;
+		int cfd = accept(sock, &saddr, &sl);
+		if(cfd < 0)
+			PrintError("accept() error", -5); 
+		pthread_mutex_lock(&m);
+		FD_SET(cfd, &fds);
+		fdID[fdNum] = cfd;
+		sprintf(names[fdNum], "username%d", fdNum);
+		write(cfd, "Your nickname: ", 15);
+		write(cfd, names[fdNum], strlen(names[fdNum]));
+		write(cfd, "\n\0", 2);
+		fdNum++;
+		maxFD = max(maxFD, cfd);
+		pthread_mutex_unlock(&m);
+	}
+}
+
+void* ReadWrite(void* p)
+{
+	struct timeval t;
+	t.tv_sec = 30;
+	t.tv_usec = 0;
+	fd_set rdset, wrset;
+	while(1)
+	{
+		pthread_mutex_lock(&m);
+		FD_ZERO(&wrset);
+		FD_ZERO(&rdset);
+		memcpy(&rdset, &fds, sizeof(fds));
+		memcpy(&wrset, &fds, sizeof(fds));
+		FD_SET(0,&rdset);
+		//FD_COPY(&fds, &rdset);
+		//FD_COPY(&fds, &wrset);
+		int n = select(maxFD + 1, &rdset, &wrset, NULL, &t);
+		if(n < 0)
+			PrintError("select() error", ERRSELECT);
+		if(n > 0)
+		{
+			char buf[1024] = {0};
+			if(FD_ISSET(0, &rdset))
+			{
+				int bc = read(0, buf, 5);
+				if(bc == 5 && strcmp(buf, "quit\n") == 0)
+					pthread_cancel(threads[1]);
+			}
+			int i = 0;
+			do
+			{
+				if(FD_ISSET(fdID[i], &rdset))
+				{
+					int bc = read(fdID[i], buf, 1024); //byte counter
+					int j;
+					for(j = 0; j < fdNum; j++)
+						if( j != i && FD_ISSET(fdID[j], &wrset))
+						{
+							write(fdID[j], names[i], strlen(names[i]));
+							write(fdID[j], ": ", 2);
+							write(fdID[j], buf, bc);
+							//write(fdID[j], "\n", 1);
+						}
+				}
+				n--;
+				i++;
+			} while(n > 0);
+		}
+		pthread_mutex_unlock(&m);
+	}
+}
+
+int main()
+{
+	memset(&sa, 0, sizeof(sa)); 
+	sa.sin_family = PF_INET;
+	sa.sin_port = htons(4400);
+	sa.sin_addr.s_addr = INADDR_ANY;
+	
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(sock < 0)
+		PrintError("socket() error", ERRSOCKET);
+	if(bind(sock, (struct sockaddr*)&sa, sizeof(sa)) < 0)
+		PrintError("bind() error", ERRBIND);
+	if(listen(sock, 10) < 0)
+		PrintError("listen() error", ERRLISTEN);
+	
+	pthread_create(threads, NULL, Accepting, NULL);
+	pthread_create(threads + 1, NULL, ReadWrite, NULL);
+	pthread_join(threads[1], NULL);
+	
+	pthread_cancel(threads[0]);
+	close(sock);
+	pthread_mutex_destroy(&m);
+	return 0;                                                                                                                           
+}
